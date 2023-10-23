809c0de4345659dd146b8f8fbe4f065d948ed78f
MrJasen <37875467+MrJasen@users.noreply.github.com>
2023-10-23 16:07:15 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oP7Yp6gqYa7SBVFKQ-8selWNhdqY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'l6ORXKJVp9O18fmr1ViEBksjw9xgp7l8SzGCEmU32Uo',
+      useTemplateId: 'p6DEM3owRNa9yY8pSlpUeDVWoug7dtRRoctQO7JyWPQ',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-03',
       festivals: [
