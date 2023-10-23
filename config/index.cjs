2522e0e57202840a4ba32ee8b8981d8558038af5
MrJasen <37875467+MrJasen@users.noreply.github.com>
2023-10-23 16:04:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,28 +23,28 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oP7Yp6gqYa7SBVFKQ-8selWNhdqY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'CBCjNr96D5a82bnJ7CHlfSf2V5j31148PmioGHy2UJQ',
+      useTemplateId: 'l6ORXKJVp9O18fmr1ViEBksjw9xgp7l8SzGCEmU32Uo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-03',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '晓钰宝贝', year: '1995', date: '08-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '玉溪', year: '1994', date: '05-14',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '09-26',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
+        // 相识的日子
+        { keyword: 'love_day', date: '2023-09-26' },
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'marry_day', date: '2023-10-21' },
       ],
     },
   ],
